96b132038c00607e89d366f146a2dc646811b2ec
marianburdianov <marian.burdianov@gmail.com>
2021-11-16 10:53:28 +0200

    update App class

--- src/main/java/com/arobs/jsontask/App.java
+++ src/main/java/com/arobs/jsontask/App.java
@@ -3,12 +3,8 @@ package com.arobs.jsontask;
 import com.arobs.jsontask.file_service.JsonFileReaderService;
 import com.arobs.jsontask.file_service.JsonFileReaderServiceImpl;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
 
         JsonFileReaderService jsonFileReaderService = new JsonFileReaderServiceImpl();
         jsonFileReaderService.getListOfBidObjectsFromJsonFile();
